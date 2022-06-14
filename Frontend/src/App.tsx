afae7cd8572f8cea0277f1971a583efd9b7c7279
Devesh Goyal <Devesh26goyal@gmail.com>
2022-06-14 12:12:58 +0530

    Added folders and dependencies

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
