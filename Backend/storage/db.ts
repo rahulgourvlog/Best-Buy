61d74e3b5f452755c6626da3019adba4684aac29
Devesh Goyal <Devesh26goyal@gmail.com>
2022-06-16 10:18:11 +0530

    added

--- Backend/storage/db.ts
+++ Backend/storage/db.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-const Connection = mongoose.connect("mongodb://localhost:27017/cart");
+const Connection = mongoose.connect("mongodb://localhost:27017/bestbuy");
 
 module.exports = Connection;
