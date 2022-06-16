e363923c04205c9cfafb9556bbd7633f01319239
Devesh Goyal <Devesh26goyal@gmail.com>
2022-06-16 10:16:21 +0530

    added

--- Frontend/src/Components/Cart/Cart.tsx
+++ Frontend/src/Components/Cart/Cart.tsx
@@ -115,7 +115,32 @@ const Cart = () => {
               </div>
 
               <div className="Cart_info-Payment">
-                <div className="Cart_info-Payment-first"></div>
+                <div className="Cart_info-Payment-first">
+                  <div
+                    className="updatePostalCode_3C6cq"
+                    data-automation="enter-postal-code"
+                  >
+                    <div className="epcHeader_fRkVs">
+                      <div className="imgCol_2IlO-">
+                        <svg
+                          className="black_139k1 deliveryIcon_CV3wI icon_3f7hE"
+                          viewBox="0 0 32 32"
+                        >
+                          <path d="M27.86,17.09H27.4l-2.24-4.55a.84.84,0,0,0-.76-.47H20.85V6.17A.85.85,0,0,0,20,5.32H5.52a.85.85,0,0,0-.85.85V8.05a.86.86,0,1,0,1.71,0V7H19.14v10H6.38v-.44a.86.86,0,1,0-1.71,0v5a2.12,2.12,0,0,0,2.12,2.12h.7a3.08,3.08,0,0,0,6.16,0h7.82a3.09,3.09,0,0,0,6.17,0h.22A2.14,2.14,0,0,0,30,21.58V19.23A2.14,2.14,0,0,0,27.86,17.09ZM10.57,25A1.39,1.39,0,1,1,12,23.59,1.38,1.38,0,0,1,10.57,25Zm14,0a1.39,1.39,0,1,1,1.39-1.38A1.38,1.38,0,0,1,24.55,25Zm3.74-3.39a.43.43,0,0,1-.43.43H27.2a3.07,3.07,0,0,0-5.3,0H13.22a3.07,3.07,0,0,0-5.3,0H6.79a.41.41,0,0,1-.41-.41V18.79H20.09A.86.86,0,0,0,21,18V13.78h2.91l2.24,4.54a.85.85,0,0,0,.77.48h1a.43.43,0,0,1,.43.43Z"></path>
+                          <path d="M9.86,13.76A.86.86,0,0,0,9,12.91H2.85a.86.86,0,0,0,0,1.72h0l6.14,0A.86.86,0,0,0,9.86,13.76Z"></path>
+                          <path d="M5.49,11.76H9A.86.86,0,1,0,9,10H5.49a.86.86,0,0,0,0,1.71Z"></path>
+                        </svg>
+                      </div>
+                      <div className="textCol_1DN1y">
+                        <h2>What's your postal code?</h2>
+                      </div>
+                    </div>
+                    <div className="epcHeaderRow_2h2kJ">
+                      It'll help us estimate shipping and delivery.
+                    </div>
+                    <div className="epcHeaderRow_2h2kJ">Change Postal Code</div>
+                  </div>
+                </div>
 
                 <div className="Cart_info-Payment-second"></div>
               </div>
