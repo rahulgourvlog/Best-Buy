0fc87c02d94d867b46c499f1a88fe46039ce9114
Devesh Goyal <Devesh26goyal@gmail.com>
2022-06-17 16:02:31 +0530

    Update cartCounter.tsx

--- Frontend/src/Context/cartCounter.tsx
+++ Frontend/src/Context/cartCounter.tsx
@@ -3,13 +3,18 @@ import React, { createContext, useState } from "react";
 const CartCount_Context = createContext({
   isChanged: true,
   setIsChanged: (value: boolean) => {},
+  isLogged: "",
+  setIsLogged: (value: string) => {},
 });
 
 const CartCount_Provider = ({ children }: React.PropsWithChildren<{}>) => {
   const [isChanged, setIsChanged] = useState<boolean>(false);
+  const [isLogged, setIsLogged] = useState("");
 
   return (
-    <CartCount_Context.Provider value={{ isChanged, setIsChanged }}>
+    <CartCount_Context.Provider
+      value={{ isChanged, setIsChanged, isLogged, setIsLogged }}
+    >
       {children}
     </CartCount_Context.Provider>
   );
